f3dbca17ed4933b5c11b0aa82e41498c23770884
HeteroCat <1580823387@qq.com>
2025-09-28 09:31:22 +0800

    Initial commit: Hello-Agents智能体学习平台

---
+++ .figma/1_897/index.jsx
@@ -0,0 +1,131 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.cyberCollege}>
+      <div className={styles.logoMenus}>
+        <div className={styles.logo2}>
+          <img src="../image/mg1oo5nw-m6pbhq0.svg" className={styles.logo} />
+          <p className={styles.helloAgents}>Hello-Agents</p>
+        </div>
+        <div className={styles.menus}>
+          <div className={styles.menu}>
+            <div className={styles.rectangle6}>
+              <div className={styles.group}>
+                <div className={styles.autoWrapper}>
+                  <div className={styles.vector} />
+                  <div className={styles.vector2} />
+                </div>
+                <div className={styles.autoWrapper}>
+                  <div className={styles.vector} />
+                  <div className={styles.vector2} />
+                </div>
+              </div>
+              <p className={styles.text}>前言</p>
+            </div>
+          </div>
+          <div className={styles.menu2}>
+            <img
+              src="../image/mg1oo5nw-sqpnfu6.svg"
+              className={styles.vuesaxLinearBook}
+            />
+            <p className={styles.text2}>第一章</p>
+          </div>
+          <div className={styles.menu2}>
+            <img
+              src="../image/mg1oo5nw-vva4hk8.svg"
+              className={styles.vuesaxLinearBook}
+            />
+            <p className={styles.text2}>第二章</p>
+          </div>
+          <div className={styles.menu2}>
+            <img
+              src="../image/mg1oo5nw-h2l99md.svg"
+              className={styles.vuesaxLinearBook}
+            />
+            <p className={styles.text2}>第三章</p>
+          </div>
+          <div className={styles.menu3}>
+            <img src="../image/mg1oo5nw-chrknz2.svg" className={styles.frame} />
+            <p className={styles.text2}>第四章</p>
+          </div>
+          <div className={styles.menu2}>
+            <img
+              src="../image/mg1oo5nw-395gvyy.svg"
+              className={styles.vuesaxLinearBook}
+            />
+            <p className={styles.text2}>第五章</p>
+          </div>
+          <div className={styles.menu2}>
+            <img
+              src="../image/mg1oo5nw-pjqyezx.svg"
+              className={styles.vuesaxLinearBook}
+            />
+            <p className={styles.text2}>第六章</p>
+          </div>
+          <div className={styles.menu2}>
+            <img
+              src="../image/mg1oo5nw-7z70s30.svg"
+              className={styles.vuesaxLinearBook}
+            />
+            <p className={styles.text2}>第七章</p>
+          </div>
+          <div className={styles.menu3}>
+            <img src="../image/mg1oo5nw-2smoepr.svg" className={styles.frame} />
+            <p className={styles.text2}>第八章</p>
+          </div>
+          <div className={styles.menu4}>
+            <img src="../image/mg1oo5nw-rca57gr.svg" className={styles.frame} />
+            <p className={styles.text3}>第九章</p>
+          </div>
+          <div className={styles.menu2}>
+            <img
+              src="../image/mg1oo5nw-hy0zmso.svg"
+              className={styles.vuesaxLinearBook}
+            />
+            <p className={styles.text2}>第十章</p>
+          </div>
+          <div className={styles.menu3}>
+            <img src="../image/mg1oo5nw-tlmkjnh.svg" className={styles.frame} />
+            <p className={styles.text2}>第十一章</p>
+          </div>
+          <div className={styles.menu3}>
+            <img src="../image/mg1oo5nw-cdwa1hs.svg" className={styles.frame} />
+            <p className={styles.text2}>第十二章</p>
+          </div>
+          <div className={styles.menu3}>
+            <img src="../image/mg1oo5nw-o93mkug.svg" className={styles.frame} />
+            <p className={styles.text2}>第十三章</p>
+          </div>
+          <div className={styles.menu2}>
+            <img
+              src="../image/mg1oo5nw-vqqt1hx.svg"
+              className={styles.vuesaxLinearBook}
+            />
+            <p className={styles.text2}>第十四章</p>
+          </div>
+          <div className={styles.menu2}>
+            <img
+              src="../image/mg1oo5nw-fq5kf2k.svg"
+              className={styles.vuesaxLinearBook}
+            />
+            <p className={styles.text2}>第十五章</p>
+          </div>
+          <div className={styles.menu3}>
+            <img src="../image/mg1oo5nw-4s13g18.svg" className={styles.frame} />
+            <p className={styles.text2}>第十六章</p>
+          </div>
+          <div className={styles.menu4}>
+            <img src="../image/mg1oo5nw-0ku7j3d.svg" className={styles.frame} />
+            <p className={styles.text3}>第十七章</p>
+          </div>
+        </div>
+      </div>
+      <p className={styles.text4}>内容</p>
+    </div>
+  );
+}
+
+export default Component;
